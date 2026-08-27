4db07f942477d8b281303a1c5e068876636913e2
Jackson <147505142+MXGao-A@users.noreply.github.com>
2026-08-27 19:52:41 +0800

    Add: subarray-sum-equals-k - submission-5

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if len(nums)==1:
+            return 1 if nums[0]==k else 0
+
+       
+        count=0
+        prefix=0
+        hash_map={0:1} 
+        #sum=prefix[i]-prefix[j-1]=k
+        #so at i, we can see how many j-1 satisfy this equation, to account for the prefix[j]==k,we need to include 0 as key and its value is 1.
+        for i,num in enumerate(nums):
+            prefix+=num
+
+            if (prefix-k) in hash_map:
+                count+=hash_map[prefix-k]
+
+            hash_map[prefix]=hash_map.get(prefix,0)+1
+        return count
